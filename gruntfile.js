31f8d2f57b7be9dfe23b0cc75f782f5c4864ab44
pb97582 <bhoitepallavi@johndeere.com>
2018-09-19 13:21:45 +0530

    chore: initial commit for workspace setup

---
+++ gruntfile.js
@@ -0,0 +1,29 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        concat: {
+            js: {
+                src: [
+                    './app/app.js',
+                    './app/components/version/*.js',
+                    './app/view1/*.js',
+                    './app/view2/*.js'
+                ],
+                dest: './app/dest/app.js'
+            }
+        },
+        uglify: {
+            js: {
+                src: ['./app/dest/app.js'],
+                dest: './app/dest/min/app.min.js'
+            }
+        }
+        //grunt task configuration will go here
+    });
+    //load grunt task
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-ng-annotate');
+    //register grunt default task
+    grunt.registerTask('default', ['ngAnnotate', 'concat', 'uglify']);
+};
